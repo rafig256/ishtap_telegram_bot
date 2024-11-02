46e792aa55f9d426a5a26fbc17b427098cb9a549
rafig256 <rafig_256@yahoo.com>
2024-11-02 19:25:42 +0330

    security data handle

--- connect.php
+++ connect.php
@@ -1,11 +1,6 @@
 <?php
-$db_host = getenv('DB_HOST');
-$db_name = getenv('DB_NAME');
-$db_user = getenv('DB_USER');
-$db_password = getenv('DB_PASSWORD');
+require_once 'config.php';
 $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8", $db_user, $db_password);
-
-
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8", $db_user, $db_password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
